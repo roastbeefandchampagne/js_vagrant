c6dd329e156e74134bf694b8818e7e89b8bb4811
cory <roastbeefandchampagne@gmail.com>
2015-05-28 04:05:11 +0200

    logstash active port on 5514

--- run_logstash_config.sh
+++ run_logstash_config.sh
@@ -1,2 +1,6 @@
-/opt/logstash/bin/logstash -f /vagrant/sys/logstash/logstash-simple.conf
+#/opt/logstash/bin/logstash -f /vagrant/sys/logstash/logstash-simple.conf
 #/opt/logstash/bin/logstash -f /vagrant/sys/logstash/logstash-minecraft.conf
+
+screen -dmS tcp /opt/logstash/bin/logstash -f /etc/logstash/conf.d/logstash-server.conf
+#screen -dmS hardware /opt/logstash/bin/logstash -f /vagrant/sys/logstash/logstash-hardware.conf
+#screen -dmS minecraft /opt/logstash/bin/logstash -f /vagrant/sys/logstash/logstash-minecraft.conf
