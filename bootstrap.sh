53b2a65b223078531629352131f1996418b53473
cory <roastbeefandchampagne@gmail.com>
2015-08-31 22:02:52 +0200

    selenium cleaning

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 # 2015
 
 echo "--RAC: ELASTICSEARCH - Selenium DEV VAGRANT INSTALL--"
-echo "--Elasticsearch + Marvel + Kibana + Logstash/forwarder + Python 2.7 + Selenium + Nginx--"
+echo "--Elasticsearch + Marvel + Kibana + Logstash + Python 2.7 + Selenium--"
 
 apt-get update
 apt-get install -y --force-yes git
@@ -18,7 +18,7 @@ apt-get install -y --force-yes build-essential
 if [ -d /vagrant/sys ]
 then
 
-	echo "--INSTALLING: Java--"
+	echo "RAC: INSTALLING Java 7"
 	apt-get install -y --force-yes openjdk-7-jre
 
 	echo "RAC: INSTALLING Elasticsearch"
@@ -41,51 +41,6 @@ then
 	cp /vagrant/sys/logstash/run_logstash_config.sh /vagrant/run_logstash_config.sh
 	chmod -R 777 /vagrant/run_logstash_config.sh
 
-	cd /vagrant
-	./start_all.sh
-
-	#installing shield
-	#echo "RAC: INSTALLING ELASTICSEARCH - Shield"
-	#cd /usr/share/elasticsearch 
-	#bin/plugin -i elasticsearch/license/latest
-	#bin/plugin -i elasticsearch/shield/latest
-
-	#bin/elasticsearch
-	#bin/shield/esusers useradd vagrant -r admin
-	#curl -XGET 'http://localhost:9200/'
-	#curl -u vagrant -XGET 'http://localhost:9200/'
-
-	#set up SSH Keys for the Logstash-forwarder
-	#mkdir -p /etc/pki/tls/certs
-	#mkdir /etc/pki/tls/private
-	#cd /etc/pki/tls
-	#sudo openssl req -x509 -batch -nodes -days 3650 -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
-	#cp /vagrant/sys/logstash/01-lumberjack-input.conf /etc/logstash/conf.d/01-lumberjack-input.conf 
-	#cp /vagrant/sys/logstash/10-syslog.conf /etc/logstash/conf.d/10-syslog.conf
-	#service logstash restart
-
-	# TODO get password inserted automaticaly
-	#echo "password for User vagrant = vagrant"
-	#scp /etc/pki/tls/certs/logstash-forwarder.crt vagrant@127.0.0.1:/tmp
-
-	#install logstash-forwarder
-	#cd /home/elasticsearch
-	#cp /vagrant/sys/logstash/forwarder/logstash-forwarder /etc/logstash-forwarder
-	#echo 'deb http://packages.elasticsearch.org/logstashforwarder/debian stable main' | sudo tee /etc/apt/sources.list.d/logstashforwarder.list
-	#wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-	#apt-get update
-	#apt-get install -y --force-yes logstash-forwarder
-	#wget https://assets.digitalocean.com/articles/logstash/logstash-forwarder_0.3.1_i386.deb
-	#dpkg -i logstash-forwarder_0.3.1_i386.deb
-	#cd /etc/init.d/
-	#wget https://raw.githubusercontent.com/elasticsearch/logstash-forwarder/a73e1cb7e43c6de97050912b5bb35910c0f8d0da/logstash-forwarder.init -O logstash-forwarder
-	#chmod +x logstash-forwarder
-	#update-rc.d logstash-forwarder defaults
-
-	#mkdir -p /etc/pki/tls/certs
-	#cp /tmp/logstash-forwarder.crt /etc/pki/tls/certs/
-	#service logstash-forwarder restart
-
 	#install kibana
 	cd /home/elasticsearch
 	wget "https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz"
@@ -93,23 +48,8 @@ then
 	cd kibana*
 	screen -dmS kibana ./bin/kibana
 
-	#install Nginx
 	cd /vagrant
-	apt-get install -y --force-yes php5-fpm
-	cp /vagrant/sys/php/php.ini /etc/php5/fpm/php.ini
-	service php5-fpm restart
-
-	apt-get install -y --force-yes nginx
-	cp /vagrant/sys/nginx/default /etc/nginx/sites-available/default
-	apt-get install -y --force-yes apache2-utils
-	#htpasswd -c /etc/nginx/conf.d/kibana.178.62.66.75.htpasswd vagrant
-	ln -s /vagrant/frames /usr/share/nginx/www/frames
-	service nginx restart
-
-	echo "RAC: INSTALLING MYSQL SERVER"
-	debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
-	debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
-	apt-get install -y --force-yes mysql-server
+	./start_all.sh
 
 	echo "RAC: INSTALLING PYTHON 2.7"
 	apt-get install -y --force-yes python2.7
@@ -136,7 +76,8 @@ then
 	pip install selenium
 
 	# install selenium
-	cd /vagrant
+	mkdir -p /home/develop/selenium
+	cd /home/develop/selenium
 	echo "RAC: INSTALLING SELENIUM SERVER"
 	wget "https://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar"
 	screen -dmS selenium java -jar selenium-server-standalone-2.39.0.jar
--- start_all.sh
+++ start_all.sh
@@ -6,7 +6,9 @@
 # roastbeefandchampagne@gmail.com
 # 2015
 
+killall -9 python
+killall -9 java
 service elasticsearch restart
 service logstash restart
-#service logstash-forwarder start
-#cd /home/elasticsearch/kibana*; screen -dmS kibana ./bin/kibana
+screen -dmS selenium java -jar /home/develop/selenium/selenium-server-standalone-2.39.0.jar
+screen -dmS kibana /home/elasticsearch/kibana*/bin/kibana
